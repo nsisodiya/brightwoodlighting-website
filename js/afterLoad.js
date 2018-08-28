1b90e66a49fe55bcd3de30c1552591a132bdf2cb
venkat <bvenkatr2@gmail.com>
2018-08-28 06:56:11 +0530

    remove query

--- js/afterLoad.js
+++ js/afterLoad.js
@@ -1,5 +1,18 @@
-$(function() {
-    var tofetch = $("load").each(function() {
-        $(this).load($(this).attr('src'));
-    });
-});
+function load(url, element)
+{
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+      let newNodes = document.createRange().createContextualFragment(this.responseText);
+      element.appendChild(newNodes);
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
+window.onload = () => {
+  let listOfNodes = Array.from(document.getElementsByTagName("load"));
+  listOfNodes.forEach((eachEle, index) => {
+    // let ele = eachEle;
+    load(eachEle.getAttribute("src"), eachEle);
+  });
+};
